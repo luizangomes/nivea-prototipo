9d6c4d6e28f79a4178924395879bf8ec07a19057
pedrojoaorp <pedrotai03@gmail.com>
2023-06-17 16:13:48 -0300

    Atualizacao interface cmd

--- classeFormulario.py
+++ classeFormulario.py
@@ -17,6 +17,23 @@ class Formulario:
                 else:
                     print("Resposta inválida. Tente novamente.")
         return relatorioFinal
+    
+    def retornarRespostas(self):
+        respostaFinal = []
+        for i in self.itens:
+            print(i.enunciado)
+            for j in i.respostas.keys():
+                print(f"{j}. {i.respostas[j]}")
+            sair = False
+            while not(sair):
+                entrada = input()
+                if entrada in i.respostas.keys():
+                    respostaFinal.append(int(entrada))
+                    sair = True
+                else:
+                    print("Resposta inválida. Tente novamente.")
+        return respostaFinal
+            
 
 """ ip1 = itemPergunta("Quantos anos você tem?", **{"1":"Tenho um ano. ", "2":"Tenho dois anos. ", "3":"Minha idade não importa. "})
 ip2 = itemPergunta("Que curso você faz?", **{"1":"Faço Engenharia da Computação. ", "2":"Faço Ciência da Computação. ", "3":"Faço Licenciatura em Computação. ", "4":"Faço Engenharia Mecatrônica. ", "5":"Sou de humanas >:]"})
